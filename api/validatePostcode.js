485b19e6b46d56d3c92ee127b8072fc475e5af7b
williamteig <william@bmedia.agency>
2024-10-26 12:06:09 +1100

    Install cors package and update changes

--- api/validatePostcode.js
+++ api/validatePostcode.js
@@ -1,6 +1,22 @@
 import https from 'https';
+import cors from 'cors';
 
 export default function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Handle OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   const { postcode } = req.query;
   
   if (!postcode) {
